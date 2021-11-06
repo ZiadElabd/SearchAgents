7ed171b2068190a9018e443e5b1f93a72398124a
Ziad Elabd <ziad.elabd91@gmail.com>
2021-11-06 14:36:43 +0200

    A* ( omar )

---
+++ src/Main/Node.java
@@ -0,0 +1,22 @@
+package Main;
+public class Node {
+	
+	//cost and heuristic
+	public int g , h  ;
+	//coordinates of blank tile 
+	public int x,y ;
+	
+	Node Parent ;
+	
+	public int[][] state = new int[3][3] ;
+	
+	public Node(int g, int h , int[][] state, int x, int y, Node p)
+	{
+		this.g = g ;
+		this.h = h ;
+		this.x = x ;
+		this.y = y ;
+		this.Parent = p ;
+		this.state = state ;
+	}
+}
