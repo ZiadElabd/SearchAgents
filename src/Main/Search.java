b03ddd77701cfb3eeb8e596f18779e897d39df14
omar rehan <omarrehan0020@gmail.com>
2021-11-12 18:20:25 +0200

    A*

--- src/Main/Search.java
+++ src/Main/Search.java
@@ -82,6 +82,12 @@ public class Search {
 	}
 	private void solve (int[][] initial, ToIntFunction<int[][]> heuristic_function) {
 		
+		if(!this.isSolvable(initial))
+		{
+			System.out.println("Un Solvable") ;
+			return ;
+		}
+		
 		int[] dx = { 1, 0, -1, 0 };
 		int[] dy = { 0, -1, 0, 1 };
 		//to get coordinates of 0 tile 
@@ -95,30 +101,18 @@ public class Search {
 					break ;
 				}
 		
-		
-		 HashMap<int[][], Integer> mp_g = new HashMap<int[][], Integer>();
-		 HashMap<int[][], Integer> mp_h = new HashMap<int[][], Integer>();
-		 HashMap<int[][], Integer> visited = new HashMap<int[][], Integer>();
+	
 		 
-		PriorityQueue<Node> pq = new PriorityQueue<Node>((a, b) -> ( mp_g.get(a.state) + mp_h.get(a.state) ) - (mp_g.get(b.state) + mp_h.get(b.state)));
+		PriorityQueue<Node> pq = new PriorityQueue<Node>((a, b) -> (a.g + a.h) - (b.g + b.h));
 		int h = heuristic_function.applyAsInt(initial) ;
 		Node node = new Node(0,h,initial,x,y,null);
 		pq.add(node) ;
-		//pq.add(new Node(0,h,initial,x,y,null));
-		/*System.out.println(pq.size());
-		pq.remove(new Node(0,h,initial,x,y,null));
-		System.out.println("pq.size()");
-		System.out.println(pq.size());*/
-		mp_g.put(initial, 0);
-		mp_h.put(initial, h);
 		while(! pq.isEmpty())
 		{
 			//System.out.println(pq.size());
 			Node parent = pq.poll() ;
-			visited.put(parent.state, 1) ;
 			if(parent.h == 0)
 			{
-				System.out.println("Done");
 				printPath(parent) ;
 				return ;
 			}
@@ -140,32 +134,10 @@ public class Search {
 					matrix[row][col] = 0 ;
 					
 					h = heuristic_function.applyAsInt(matrix) ;
-					if(visited.containsKey(matrix))
-					{
-						System.out.println("vfdvfd");
-						continue;
-					}
-					if(mp_g.containsKey(matrix))
-					{
-						int newCost = h + parent.g ;
-						int oldCost =mp_g.get(matrix) + mp_h.get(matrix) ;
-						if(oldCost <= newCost)
-							continue ;
-						else
-						{
-							mp_g.put(matrix,parent.g + 1);
-							pq.remove(new Node(parent.g + 1,h,matrix,row,col,null));
-							pq.add(new Node(parent.g + 1,h,matrix,row,col,parent));
-						}
-
-					}
-					else
-					{
-						mp_g.put(matrix,parent.g + 1);
-						mp_h.put(matrix, h);
-						Node newNode = new Node(parent.g + 1,h,matrix,row,col,parent);
-						pq.add(newNode) ;
-					}
+							
+					Node newNode = new Node(parent.g + 1,h,matrix,row,col,parent);
+					pq.add(newNode) ;
+					
 				}
 			}
 		}
@@ -177,7 +149,7 @@ public class Search {
 	
 	
 	public static void main(String[] args) {
-		int[][] initial = { {4, 3, 2}, {6, 5, 0}, {7, 8, 1} };
+		int[][] initial = { {2, 1, 4}, {6, 0, 5}, {3, 8, 7} };
 		
 		Search s = new Search() ;
 		
